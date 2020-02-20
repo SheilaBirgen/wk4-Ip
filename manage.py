c399c87d12ca987b7600acc10d98d1fafc96314f
SheilaBirgen <jeronobergen@gmail.com>
2020-02-20 13:51:20 +0300

    upgraded the changes available

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate, MigrateCommand
 
 
 #  Creating app instance
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
@@ -21,7 +21,7 @@ def test():
     """
     import unittest
     tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+    unittest.TextTestRunnetr(verbosity=2).run(tests)
 
 @manager.shell
 def make_shell_context():
